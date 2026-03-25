64728db87388b0c8fbe2947f99fa2a01b0f1ccc4
Pedro-Azen <115229741+Pedro-Azen@users.noreply.github.com>
2026-03-25 16:12:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 int conta_pares(int n, int mat[][n]) {
     /* TODO: Implemente esta função.
      * Para verificar se um número é par, use a expressão (x % 2 == 0) */
+    int i, j;
+    int numPares = 0;
+     for (i = 0; i < n; i++) {
+        for(j = 0; j <= i; j++) {
+            if (mat[i][j] %2 == 0){
+                numPares++;
+            }
+        }
+     }
+     return numPares;
 }
 
 int main() {
